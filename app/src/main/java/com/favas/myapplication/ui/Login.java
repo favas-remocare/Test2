9217f3536463436d03c07916e8083101fe00662d
favas <favas@remo.care>
2020-07-14 17:29:12 +0530

    added yaml filegf

--- app/src/main/java/com/favas/myapplication/ui/Login.java
+++ app/src/main/java/com/favas/myapplication/ui/Login.java
@@ -12,5 +12,7 @@ public class Login extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        
     }
 }
